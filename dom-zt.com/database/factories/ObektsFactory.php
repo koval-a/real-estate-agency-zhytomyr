6889b815243388fb8ace5e3039ffdcdcf5e8861f
Yaroslav Lukyanchuk <yarikthe@gmail.com>
2021-07-09 03:39:30 +0300

    data

--- dom-zt.com/database/factories/ObektsFactory.php
+++ dom-zt.com/database/factories/ObektsFactory.php
@@ -25,14 +25,14 @@ class ObektsFactory extends Factory
         return [
             'name' => $this->faker->text(30),
             'description' => $this->faker->paragraph,
-            'price' => $this->faker->numberBetween(1000, 999999999),
+            'price' => $this->faker->numberBetween(1000, 9999),
             'category_id' => $this->faker->numberBetween(1, 4),
             'location_rayon_id' => $this->faker->numberBetween(51, 75),
             'location_city_id' => $this->faker->numberBetween(3, 32),
             'location_city_rayon_id' => $this->faker->numberBetween(10, 33),
             'note' => $this->faker->paragraph,
             'address' => $this->faker->address,
-            'square' => $this->faker->numberBetween(10, 20000),
+            'square' => $this->faker->numberBetween(10, 200),
             'main_img' => $this->faker->image(public_path('/files/images/obekts/faker'),640,480, null, false),
             'isPublic' => $this->faker->numberBetween(0, 1),
             'count_room' => $this->faker->numberBetween(1, 20),
@@ -40,12 +40,12 @@ class ObektsFactory extends Factory
             'level' => $this->faker->numberBetween(1, 100),
             'opalenyaName' => $this->faker->randomElement(['Автономне', 'Центральне']),
             'appointment_id' => $this->faker->numberBetween(17, 46),
-            'rieltor_id' => $this->faker->numberBetween(2, 20),
+            'rieltor_id' => 10,
             'slug' => $this->faker->slug,
-            'owner_id' => $this->faker->numberBetween(11, 1000),
+            'owner_id' => $this->faker->numberBetween(11, 100),
             'isPay' => $this->faker->numberBetween(0, 1),
             'type_wall_id' => $this->faker->numberBetween(1, 8),
-            'square_hause_land' => $this->faker->numberBetween(5, 1000),
+            'square_hause_land' => $this->faker->numberBetween(5, 100),
         ];
     }
 }
--- dom-zt.com/resources/views/pages/all-obekts.blade.php
+++ dom-zt.com/resources/views/pages/all-obekts.blade.php
@@ -689,7 +689,12 @@
 {{--            </div>--}}
             <div class="col-md-9" style="margin-left: 30%; min-height: 800px;">
 
-
+{{--                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
+{{--                <!-- news-sidbar -->--}}
+{{--                <ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-4453172390299200" data-ad-slot="1427392196" data-ad-format="auto" data-full-width-responsive="true"></ins>--}}
+{{--                <script>--}}
+{{--                    (adsbygoogle = window.adsbygoogle || []).push({});--}}
+{{--                </script>--}}
 
             </div>
         </div>
